3c4c378589f6b175c0e93b06491a6d1d4534a599
kanapka81 <kanapkaa81@gmail.com>
2018-09-03 16:19:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -19,3 +20,15 @@ io.on('connection', function(socket){
 http.listen(5000, function(){
   console.log('listening on *:5000');
 })
+
+*/
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+ express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
